ad327e2e99aaf40692327c44e55c4ebc703808be
yoshikitatsumi <66803007+yoshikitatsumi@users.noreply.github.com>
2020-06-28 13:35:22 +1200

    Add files via upload

---
+++ UnitTest1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Assignment1;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestRussianRoulette
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        
+        RRClass RR = new RRClass();
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int x = RR.Bullet;
+            Assert.AreNotEqual(x, 6);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            int y = RR.Point;
+            Assert.AreEqual(y, 0);
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            int z = RR.Shot;
+            Assert.AreEqual(z, RR.Shot1 + RR.Shot2);
+        }
+        [TestMethod]
+        public void TestMethod4()
+        {
+            int a = RR.Shot;
+            Assert.AreNotEqual(a, 6);
+        }
+    }
+}
